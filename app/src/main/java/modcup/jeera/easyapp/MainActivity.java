c4a6c2c2b84d276ed69fbd3cff7e3f233788935f
Jeera <modcup365>
2017-10-07 15:33:19 +0700

    Create_First_Fragment

--- app/src/main/java/modcup/jeera/easyapp/MainActivity.java
+++ app/src/main/java/modcup/jeera/easyapp/MainActivity.java
@@ -3,11 +3,20 @@ package modcup.jeera.easyapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import modcup.jeera.easyapp.fragment.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);     //Save instanceState is resume application to continue
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Add Fragment Activities
+        if(savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().add(R.id.contentFragmentMain,new MainFragment()).commit();
+
+        }
+
+    } // Main Method  :: Every app start from this method
+}// Main Class
